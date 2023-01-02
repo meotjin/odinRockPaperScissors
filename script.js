3b07b4821a9a000d9966a2e342425131d31e6a89
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-02 10:12:33 +0330

    remove 5time play loop and create buttons html

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-let options = ['rock', 'paper', 'scissors'];
+let options = ["rock", "paper", "scissors"];
 
 let userScore = 0,
-    computerScore = 0;
+	computerScore = 0;
 
-function getComputerChoice(){
-    let randomNum = Math.floor(Math.random()*3);
-    return options[randomNum];
+function getComputerChoice() {
+	let randomNum = Math.floor(Math.random() * 3);
+	return options[randomNum];
 }
 
 function winner(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    //make a key thats shows both players choices
-    let key = options.indexOf(playerSelection) + '' + options.indexOf(computerSelection);
-    switch (key) {
-        case '00':
-        case '11':
-        case '22':
-            return `Draw! both players chose ${playerSelection}.`;
-        case '02':
-        case '10':
-        case '21':
-            userScore++;
-            return `You Won! ${playerSelection} beats ${computerSelection}.`;
-        default:
-            computerScore++;
-            return `You Lost :(! ${computerSelection} beats ${playerSelection}.`;   
-    }
+	playerSelection = playerSelection.toLowerCase();
+	//make a key thats shows both players choices
+	let key =
+		options.indexOf(playerSelection) + "" + options.indexOf(computerSelection);
+	switch (key) {
+		case "00":
+		case "11":
+		case "22":
+			return `Draw! both players chose ${playerSelection}.`;
+		case "02":
+		case "10":
+		case "21":
+			userScore++;
+			return `You Won! ${playerSelection} beats ${computerSelection}.`;
+		default:
+			computerScore++;
+			return `You Lost :(! ${computerSelection} beats ${playerSelection}.`;
+	}
 }
 
-
-function game(){
-    for (let index = 0; index < 5; index++) {
-        console.log(winner(prompt("rock/paper or scissors?"), getComputerChoice()));
-    }
-    console.log(`user score: ${userScore}\ncomputer score: ${computerScore}`);
-    console.log(userScore>computerScore ? "you won! congrats!" : userScore < computerScore ? "you lost :( try again!" : "Draw!");
+function game() {
+	console.log(`user score: ${userScore}\ncomputer score: ${computerScore}`);
+	console.log(
+		userScore > computerScore
+			? "you won! congrats!"
+			: userScore < computerScore
+			? "you lost :( try again!"
+			: "Draw!"
+	);
 }
 
 game();
